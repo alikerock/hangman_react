f54993d6d49d9505b2ac35fcafc80e7055b13530
rock's ezweb <alikerock@gmail.com>
2024-11-01 12:55:15 +0900

    setWord form add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import './App.css';
 import GameBoard from './GameBoard';
+import SetWord from './SetWord';
 
 function App() {
   const [secretWord, setSecretWord] = useState('');
+  const [maxError, setMaxError] = useState(0);
   return (
     <div className="App">
      <h1>Hangman</h1>
      <p>Do you want to play game</p>
      <div>
-      <GameBoard secretWord={secretWord} maxError={8} />
+      <GameBoard secretWord={secretWord} maxError={maxError} />
+      {!secretWord &&  <SetWord wordSetted={(val)=>{
+        setSecretWord(val);
+        setMaxError(val.length + 2);
+      }}/>}
+     
      </div>
     </div>
   );
--- src/SetWord.js
+++ src/SetWord.js
@@ -1,7 +1,16 @@
-import { useState } from 'react';
+import React from 'react';
 
-let SetWord = ()=>{
+let SetWord = ({wordSetted})=>{
 
+  return(
+    <form onSubmit={(e)=>{
+      e.preventDefault();
+      wordSetted(e.target.word.value);
+    }}>
+      <input type="text" name="word"/>
+      <button type="submit">입력</button>
+    </form>
+  )
 }
 
 export default SetWord;
