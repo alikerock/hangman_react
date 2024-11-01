d7296ccb7f775dbff251c00c52c3c32ed026f0aa
rock's ezweb <alikerock@gmail.com>
2024-11-01 13:20:20 +0900

    router 추가

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import { useState } from 'react';
 import './App.css';
 import GameBoard from './GameBoard';
 import SetWord from './SetWord';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [secretWord, setSecretWord] = useState('');
   const [maxError, setMaxError] = useState(0);
   return (
     <div className="App">
-     <h1>Hangman</h1>
-     <p>Do you want to play game</p>
-     <div>
-      <GameBoard secretWord={secretWord} maxError={maxError} />
-      {!secretWord &&  <SetWord wordSetted={(val)=>{
+      <h1>Hangman</h1>
+      <p>Do you want to play game</p>
+
+      <Routes>
+        <Route path="/" element={<GameBoard secretWord={secretWord} maxError={maxError} />} />
+        <Route path="/admin" element={<SetWord wordSetted={(val)=>{
         setSecretWord(val);
         setMaxError(val.length + 2);
-      }}/>}
-     
-     </div>
+      }}/>} />
+      </Routes> 
+      
     </div>
   );
 }
