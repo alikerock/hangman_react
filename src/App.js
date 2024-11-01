c5fc6bcd5a4f457f62e10332c415a021022d8d95
rock's ezweb <alikerock@gmail.com>
2024-11-01 22:12:02 +0900

    Update App.js
    로컬스토리지에서 여러 단어 불러오도록

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import GameBoard from './GameBoard';
 import SetWord from './SetWord';
@@ -10,19 +10,36 @@ import {
 function App() {
   const [maxError, setMaxError] = useState(0);
   const [answerLength, setAnswerLength] = useState(0);
-  const [secretWord, setSecretWord] = useState('');
-
-  useEffect(()=>{
-    let word = window.localStorage.getItem('scretWord');    
-    if(word && word.length > 0 ){
-      setSecretWord(word);
-      setAnswerLength(word.length);
-      setMaxError(word.length + 2)
-    }else{
+  const [secretWord, setSecretWord] = useState();
+  const [cid, setCid] = useState(0);
+  /*
+  let words = ['react', 'vue', 'angular'];
+  let wordString = JSON.stringify(words);
+  window.localStorage.setItem('scretWord', wordString);
+  */
+  let setGame = useCallback(() => {
+    let localCID = window.localStorage.getItem('cid');
+    if (localCID && localCID > cid) {
+      setCid(localCID);
+    }
+    let wordArrStr = window.localStorage.getItem('scretWord');
+    if (wordArrStr && wordArrStr.length > 0) {
+      let wordArr = JSON.parse(wordArrStr);
+      setSecretWord(wordArr[cid]);
+      setAnswerLength(wordArr[cid].length);
+      setMaxError(wordArr[cid].length + 2)
+    } else {
       setAnswerLength(0);
       setMaxError(1)
     }
-  },[]);
+  }, [cid]);
+
+  useEffect(() => {
+    console.log('setGame 실행');
+    setGame();
+  }, [setGame]);
+
+
 
   return (
     <div className="App">
@@ -30,10 +47,11 @@ function App() {
       <p>Do you want to play game</p>
 
       <Routes>
-        <Route path="/" element={<GameBoard secretWord={secretWord} maxError={maxError} answerLength={answerLength}/>} />
+        <Route path="/" element={<GameBoard secretWord={secretWord} maxError={maxError} answerLength={answerLength} setGame={setGame}
+        />} />
         <Route path="/admin" element={<SetWord />} />
-      </Routes> 
-      
+      </Routes>
+
     </div>
   );
 }
