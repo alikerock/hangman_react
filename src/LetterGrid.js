d731a456fd9c2f010b1e1a29d417f806af9c06e2
rock's ezweb <alikerock@gmail.com>
2024-11-01 22:13:20 +0900

    Update LetterGrid.js
    정답시 다음 단어로 세팅할 수 있도록

--- src/LetterGrid.js
+++ src/LetterGrid.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Letter from './Letter';
 
-const LetterGrid = ({secretWord, guessedLetters, answerLength, complete})=>{
+const LetterGrid = ({ secretWord, guessedLetters, answerLength, complete }) => {
 
   //let letters = secretWord.split('').map(letter=><span>{letter}</span>);
 
@@ -11,39 +11,54 @@ const LetterGrid = ({secretWord, guessedLetters, answerLength, complete})=>{
 
 
   //answer값이 변경되면 answerLength와 비교해서 정답여부 파악
-  useEffect(()=>{
-    if(answerLength > 0 && answer === answerLength && !isComplete){
-      alert('정답입니다!');
+  useEffect(() => {
+    if (answerLength > 0 && answer === answerLength && !isComplete) {
+      let localCID = Number(window.localStorage.getItem('cid'));
+      let wordArrStr = window.localStorage.getItem('scretWord');
+      let wordArr = JSON.parse(wordArrStr);
+      let wordArrlength = wordArr.length;
+      if (localCID < wordArrlength - 1) {
+        let nextCID = localCID + 1;
+        let nextCIDstr = JSON.stringify(nextCID);
+        window.localStorage.setItem('cid', nextCIDstr);
+        alert('정답입니다!');
+      } else {
+        alert('모든 문제 풀이를 마쳤습니다.');
+        window.localStorage.setItem('cid', 0);
+      }
       complete();
       setIsComplete(true);
+      setTimeout(() => {
+        setIsComplete(false);
+      }, 500)
     }
-  },[answer, answerLength, complete, isComplete]);
+  }, [answer, answerLength, complete, isComplete]);
 
   //guessedLetters값이 변경되면 answer를 업데이트
-  useEffect(()=>{
-    
-    let newCount = [...secretWord].reduce((count,letter)=>{
-      return count + (guessedLetters.includes(letter.toLowerCase())? 1:0);
-    },0);
+  useEffect(() => {
+
+    let newCount = [...secretWord].reduce((count, letter) => {
+      return count + (guessedLetters.includes(letter.toLowerCase()) ? 1 : 0);
+    }, 0);
     setAnswer(newCount);
 
     console.log('실행', newCount);
 
-  },[guessedLetters, secretWord])
+  }, [guessedLetters, secretWord])
 
-  let letters = [...secretWord].map((letter, idx)=>{
+  let letters = [...secretWord].map((letter, idx) => {
     //let isShown = guessedLetters.indexOf(letter.toLowerCase()) > -1;
     let isShown = guessedLetters.includes(letter.toLowerCase());
-    return(
+    return (
       <Letter key={idx} value={letter} isShown={isShown}></Letter>
     )
-});
-  
+  });
+
   return (
     <div className="letters">
 
       {letters}
-     
+
     </div>
   );
 }
