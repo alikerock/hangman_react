2a095d04ca59b00b4b1d551b70eee04f22161d15
rock's ezweb <alikerock@gmail.com>
2024-11-01 22:11:36 +0900

    Update SetWord.js
    여러단어 입력, 스테이지 구성

--- src/SetWord.js
+++ src/SetWord.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-let SetWord = ()=>{
-  return(
-    <form onSubmit={(e)=>{
+let SetWord = () => {
+  return (
+    <form onSubmit={(e) => {
       e.preventDefault();
-      window.localStorage.setItem('scretWord', e.target.word.value);
+      let wordArr = e.target.word.value.split(',');
+      let wordArrSTR = JSON.stringify(wordArr);
+      window.localStorage.setItem('scretWord', wordArrSTR);
     }}>
-      <input type="text" name="word"/>
+      <p>문항을 콤마로 나열하여 입력하세요</p>
+      <input type="text" name="word" placeholder='react, vue, type' />
       <button type="submit">입력</button>
     </form>
   )
